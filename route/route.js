60c9d2959284899e8b39e8d8e033fddde35e5839
samironbhowmik <bhowmiksamiron@gmail.com>
2023-02-07 16:22:02 +0530

    added signup and signin route and generated token

--- route/route.js
+++ route/route.js
@@ -1,20 +1,31 @@
 const router = require("express").Router()
 const bcrypt = require("bcrypt");
 const userModel = require("../model/user");
+const jwt = require("jsonwebtoken")
 
-router.post("/login", async(req,res)=>{
+//signup
+router.post("/signup", async(req,res)=>{
     try {
         const {email,password} = req.body
         // console.log(email,password);
+        const existingUser = await userModel.findOne({email:email})
+        if(existingUser){
+            return res.json({
+                message:"User already exists"
+            })
+        }
 
-        const newPassword = await bcrypt.hash(password,10)
-        const allUser = await userModel.create({
+        const hashedPassword = await bcrypt.hash(password,10)
+        const result = await userModel.create({
             email:email,
-            password:newPassword
+            password:hashedPassword
         })
+
+        const token = jwt.sign({email:result.email, id:result._id}, process.env.MY_JWT)
         res.json({
             status:"success",
-            allUser
+            email:result,
+            token:token
         })
         
     } catch (error) {
@@ -24,5 +35,40 @@ router.post("/login", async(req,res)=>{
         })
     }
 })
+
+//signin
+router.post("/signin",async(req,res)=>{
+    try {
+        let {email, password} = req.body
+        const existingUser = await userModel.findOne({email:email})
+        if(!existingUser){
+            return res.json({
+                message:"Cannot find user"
+            })
+        }
+
+        const matchPassword = await bcrypt.compare(password,existingUser.password)
+        if(!matchPassword)
+        {
+            return res.json({
+                message:"Wrong password"
+            })
+        }
+
+        const token = jwt.sign({email:existingUser.email, id:existingUser._id}, process.env.MY_JWT)
+        res.json({
+            status:"success",
+            email:existingUser,
+            token:token
+        })
+        
+    } catch (error) {
+        res.json({
+            status:"success",
+            message:error.message
+        })
+    }
+})
+
 
 module.exports = router
